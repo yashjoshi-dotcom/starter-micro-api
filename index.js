891a315618ef3fd857e36c3ce1521427de713025
Yash Joshi <77540753+yashjoshi-dotcom@users.noreply.github.com>
2023-02-02 00:43:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+const fs =require("fs");
+const server = http.createServer(function(req, res) 
+{ 
+res.writeHead(200, {'Content-type' : 'text/css'});
+var fileContents = fs.readFileSync('./style.css', {encoding: 'utf8'});
+res.write(fileContents);
+res.end();
+})
+
+server.listen(port, function(error) {
+if (error) {
+console.log('Something went wrong', error)
+} else {
+    console.log('Server is listening on port ' + port)
+}
+})
